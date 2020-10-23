7fff77c65ab8f30b60db467d3150845fe654fefd
Kennette Canales <kennettecanales@gmail.com>
2020-10-23 10:03:39 +0800

    udpate

--- _defaultApp.js
+++ _defaultApp.js
@@ -7,7 +7,6 @@ import AppNavigation from 'navigation';
 import { createAppContainer } from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
 import { Helper } from 'common';
-import { Tutorial } from 'components';
 const AppContainer = createAppContainer(AppNavigation);
 
 function ReduxNavigation (props) {
@@ -15,53 +14,25 @@ function ReduxNavigation (props) {
 }
 
 const mapStateToProps = state => ({ state: state })
-let AppReduxNavigation = connect(mapStateToProps)(ReduxNavigation)
-const store = createStore(rootReducer);
+const mapDispatchToProps = dispatch => {
+  const { actions } = require('@redux');
+  return {
+    setTheme: (theme) => dispatch(actions.setTheme(theme))
+  };
+};
+let AppReduxNavigation = connect(mapStateToProps, mapDispatchToProps)(ReduxNavigation)
+export const store = createStore(rootReducer);
 
 export default class App extends React.Component{
   constructor(props) {
     super(props);
-  
-    this.state = {
-      tutorial: false
-    };
   }
 
   componentDidMount(){
-    this.checkTutorial();
-  }
-
-  checkTutorial = async () => {
-    try {
-      const flag = await AsyncStorage.getItem(Helper.APP_NAME + 'tutorial');
-      console.log('flag', flag)
-      if(flag != null) {
-        this.setState({tutorial: true})
-      }
-    } catch(e) {
-      console.log(e)
-    }
-  }
-  
-  storeData = async (key, value) => {
-    try {
-      await AsyncStorage.setItem(`${Helper.APP_NAME}${key}`, value)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  onFinish = () => {
-    this.storeData('tutorial', 'done');
-    this.setState({tutorial: true})
-  }
-
-  onSkip = () => {
-    console.log('onSkip')
+    // this.getTheme()
   }
 
   render() {
-    const { tutorial } = this.state;
     console.ignoredYellowBox = ['Warning: Each'];
     return (
       <Provider store={store}>
@@ -69,17 +40,7 @@ export default class App extends React.Component{
             flex: 1,
             backgroundColor: '#ffffff'
           }}>
-          {
-            tutorial == true && (
-              <AppReduxNavigation />
-            )
-          }
-          {
-            tutorial == false && (
-              <Tutorial onFinish={() => this.onFinish()} onSkip={() => this.onSkip()}/>
-            )
-          }
-         
+            <AppReduxNavigation />
         </View>
       </Provider>
     );

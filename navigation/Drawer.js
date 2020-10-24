3dd1c5aa4e3db648a2459f4359b3f3ed39217330
Kennette Canales <kennettecanales@gmail.com>
2020-10-24 11:33:07 +0800

    update

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import Slider from 'components/slider';
+import Slider from 'components/Slider/index.js';
 import { Color, BasicStyles } from 'common';
 import Dashboard from 'modules/basics/Welcome.js';
 import OptionRight from './OptionRight'

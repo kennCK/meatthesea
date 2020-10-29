0c682aa2e4215965a87731eb06fb3fdedfed0cfd
goriko <guanzonedrian@gmail.com>
2020-10-29 21:34:31 +0800

    new updates product page

--- modules/products/components/style.js
+++ modules/products/components/style.js
@@ -2,62 +2,62 @@ import { Color } from 'common';
 import { Dimensions } from 'react-native';
 const width = Math.round(Dimensions.get('window').width);
 export default {
-    scrollContainer: {
-        alignItems: 'center',
-        width: '100%',
-        height: '100%'
-    },
-    imageRow: {
-        width: '100%',
-        paddingTop: 5,
-        paddingBottom: 5,
-        flexDirection: 'row'
-    },
-    productImage: {
-        width: (width/3)-20,
-        height: (width/3)-20
-    },
-    productContainer: {
-        width: (width/3),
-        alignItems: 'center',
-    },
-    productName: {
-        textAlign: 'center',
-        fontWeight: "bold",
-        fontSize: 18
-    },
-    menuContainer: {
-        flexDirection: 'row',
-        width: '100%'
-    },
-    menuItem: {
-        width: '50%',
-        borderWidth: 1,
-        paddingTop: 5,
-        borderColor: Color.gray,
-        alignItems: 'center'
-    },
-    menuTextSelected: {
-        fontWeight: 'bold',
-        color: Color.primary,
-        fontSize: 20
-    },
-    menuText: {
-        fontSize: 20
-    },
-    rectangleSelected: {
-        width: '100%',
-        height: 5,
-        backgroundColor: Color.primary
-    },
-    rectangle: {
-        width: '100%',
-        height: 5
-    },
-    showScroll: {
-        visibility: 'visible'
-    },
-    hideScroll: {
-        visibility: 'hidden'
-    }
+  scrollContainer: {
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  imageRow: {
+    width: '100%',
+    paddingTop: 5,
+    paddingBottom: 5,
+    flexDirection: 'row'
+  },
+  productImage: {
+    width: (width/3)-20,
+    height: (width/3)-20
+  },
+  productContainer: {
+    width: (width/3),
+    alignItems: 'center',
+  },
+  productName: {
+    textAlign: 'center',
+    fontWeight: "bold",
+    fontSize: 18
+  },
+  menuContainer: {
+    flexDirection: 'row',
+    width: '100%'
+  },
+  menuItem: {
+    width: '50%',
+    borderWidth: 1,
+    paddingTop: 5,
+    borderColor: Color.gray,
+    alignItems: 'center'
+  },
+  menuTextSelected: {
+    fontWeight: 'bold',
+    color: Color.primary,
+    fontSize: 20
+  },
+  menuText: {
+    fontSize: 20
+  },
+  rectangleSelected: {
+    width: '100%',
+    height: 5,
+    backgroundColor: Color.primary
+  },
+  rectangle: {
+    width: '100%',
+    height: 5
+  },
+  showScroll: {
+    visibility: 'visible'
+  },
+  hideScroll: {
+    visibility: 'hidden'
+  }
 }

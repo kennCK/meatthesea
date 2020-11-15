26fe9bc556099b65e5e1960beff69b3674467b67
keenanmendiola <keenanpaulmendiola@gmail.com>
2020-11-15 12:47:00 +0800

    Merge

--- common/Routes.js
+++ common/Routes.js
@@ -5,19 +5,12 @@ export default {
   // Categories
   categoriesRetrieve: apiUrl + 'categories',
   categoriesAdd: apiUrl + 'categories',
-<<<<<<< HEAD
-  categoriesCount: apiUrl + 'categories​/count',
-  categoriesRetrieveById: id => apiUrl + `categories​/${id}`,
-  categoriesUpdateById: id => apiUrl + `categories​/${id}`,
-  categoriesDeleteById: id => apiUrl + `categories​/${id}`,
-=======
   categoriesCount: apiUrl + 'categories/count',
   categoriesRetrieveById: id => apiUrl + 'categories' + id,
   categoriesUpdateById: id => apiUrl + 'categories/' + id,
   categoriesDeleteById: id => apiUrl + 'categories/' + id,
   restaurantCategoriesRetrieve: apiUrl + 'get_restaurant_categories',
   deliCategoriesRetrieve: apiUrl + 'get_deli_shop_categories',
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d
 
   // CustomerRoles
   customerRolesRetrieve: apiUrl + 'customer_roles',
@@ -43,34 +36,8 @@ export default {
   manufacturerDeleteById: id => apiUrl + 'manufacturers/' + id,
   // NewsLetterSubscription
   newsLetterSubscriptionRetrieve: apiUrl + 'news_letter_subscriptions',
-<<<<<<< HEAD
   newsLetterSubscriptionDeactivate: email =>
-    apiUrl + `news_letter_subscriptions​/${email}​/deactivate`,
-
-  // OrderItems
-
-  orderItemsRetrieve: orderId => apiUrl + `orders​/${orderId}​/items`,
-  orderItemsAdd: orderId => `orders​/${orderId}​/items`,
-  orderItemsDeleteByOrderId: orderId => apiUrl + `orders​/${orderId}​/items`,
-  orderItemsCount: orderId => `orders​/${orderId}​/items​/count`,
-  orderItemsRetrieveWithDetails: (orderId, orderItemId) =>
-    apiUrl + `orders​/${orderId}​/items​/${orderItemId}`,
-  orderItemsUpdate: (orderId, orderItemId) =>
-    apiUrl + `orders​/${orderId}​/items​/${orderItemId}`,
-  orderItemsDeleteByOrderItemId: (orderId, orderItemId) =>
-    `orders​/${orderId}​/items​/${orderItemId}`,
-
-  // Orders
-  ordersRetrieve: apiUrl + 'orders',
-  ordersAdd: 'orders',
-  ordersCount: 'orders​/count',
-  ordersRetrieveById: id => apiUrl + `orders​/${id}`,
-  ordersDeleteById: id => apiUrl + `orders​/${id}`,
-  ordersUpdateById: id => apiUrl + `orders​/${id}`,
-  ordersRetrieveByCustomer: customer_id =>
-    apiUrl + `orders​/customer​/${customer_id}`,
-=======
-  newsLetterSubscriptionDeactivate: email => apiUrl + 'news_letter_subscriptions/' + email + '/deactivate',
+    apiUrl + 'news_letter_subscriptions/' + email + '/deactivate',
 
   // OrderItems
 
@@ -78,19 +45,22 @@ export default {
   orderItemsAdd: orderId => 'orders/' + orderId + '/items',
   orderItemsDeleteByOrderId: orderId => apiUrl + 'orders/' + orderId + '/items',
   orderItemsCount: orderId => 'orders/' + orderId + '/items/count',
-  orderItemsRetrieveWithDetails: (orderId, orderItemId) => apiUrl + 'orders/' + orderId + '/items/' + orderItemId,
-  orderItemsUpdate: (orderId, orderItemId) => apiUrl + 'orders/' + orderId + '/items/' + orderItemId,
-  orderItemsDeleteByOrderItemId: (orderId, orderItemId) => 'orders/' + orderId + '/items/' + orderItemId,
+  orderItemsRetrieveWithDetails: (orderId, orderItemId) =>
+    apiUrl + 'orders/' + orderId + '/items/' + orderItemId,
+  orderItemsUpdate: (orderId, orderItemId) =>
+    apiUrl + 'orders/' + orderId + '/items/' + orderItemId,
+  orderItemsDeleteByOrderItemId: (orderId, orderItemId) =>
+    'orders/' + orderId + '/items/' + orderItemId,
 
   // Orders
-  ordersRetrieve: apiUrl + "orders",
-  ordersAdd:  apiUrl +'orders',
+  ordersRetrieve: apiUrl + 'orders',
+  ordersAdd: apiUrl + 'orders',
   ordersCount: apiUrl + 'orders/count',
   ordersRetrieveById: id => apiUrl + 'orders/' + id,
   ordersDeleteById: id => apiUrl + 'orders/' + id,
   ordersUpdateById: id => apiUrl + 'orders/' + id,
-  ordersRetrieveByCustomer: customer_id => apiUrl + 'orders/customer/' + customer_id,
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d
+  ordersRetrieveByCustomer: customer_id =>
+    apiUrl + 'orders/customer/' + customer_id,
 
   // ProductAttributes
   productAttribuesRetrieve: apiUrl + 'productattributes',
@@ -110,71 +80,43 @@ export default {
   // ProductManufacturerMappings
   productManufacturerMappingsRetrieve: apiUrl + 'product_manufacturer_mappings',
   productManufacturerMappingsAdd: apiUrl + 'product_manufacturer_mappings',
-<<<<<<< HEAD
   productManufacturerMappingsCount:
     apiUrl + 'product_manufacturer_mappings/count',
   productManufacturerMappingsRetrieveById: id =>
-    `product_manufacturer_mappings/${id}`,
+    'product_manufacturer_mappings/' + id,
   productManufacturerMappingsUpdate: id =>
-    `product_manufacturer_mappings/${id}`,
+    apiUrl + 'product_manufacturer_mappings/' + id,
   productManufacturerMappingsDelete: id =>
-    `product_manufacturer_mappings/${id}`,
-=======
-  productManufacturerMappingsCount: apiUrl + 'product_manufacturer_mappings/count',
-  productManufacturerMappingsRetrieveById: id => 'product_manufacturer_mappings/' + id,
-  productManufacturerMappingsUpdate: id =>  apiUrl +'product_manufacturer_mappings/' + id,
-  productManufacturerMappingsDelete: id =>  apiUrl +'product_manufacturer_mappings/' + id,
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d
+    apiUrl + 'product_manufacturer_mappings/' + id,
 
   // Products
   productsRetrieve: apiUrl + 'products',
   productsAdd: apiUrl + 'products',
-<<<<<<< HEAD
-  productsCount: apiUrl + 'products​/count',
-  productsRetrieveById: id => apiUrl + `products​/${id}`,
-  productsUpdateById: id => apiUrl + `products​/${id}`,
-  productsDeleteById: id => apiUrl + `products​/${id}`,
-=======
   productsCount: apiUrl + 'products/count',
   productsRetrieveById: id => apiUrl + 'products/' + id,
   productsUpdateById: id => apiUrl + 'products/' + id,
   productsDeleteById: id => apiUrl + 'products/' + id,
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d
 
   // ProductSpecificationAttributes
   productSpecificationAttributesRetrieve:
     apiUrl + 'productspecificationattributes',
   productSpecificationAttributesAdd: apiUrl + 'productspecificationattributes',
-<<<<<<< HEAD
   productSpecificationAttributesCount:
     apiUrl + 'productspecificationattributes/count',
   productSpecificationAttributesRetrieveById: id =>
-    `productspecificationattributes/${id}`,
+    'productspecificationattributes/' + id,
   productSpecificationAttributesUpdate: id =>
-    `productspecificationattributes/${id}`,
+    'productspecificationattributes/' + id,
   productSpecificationAttributesDelete: id =>
-    `productspecificationattributes/${id}`,
+    'productspecificationattributes/' + id,
 
   // ShoppingCartItems
-  shoppingCartItemsRetrieve: 'shopping_cart_items',
-  shoppingCartItemsAdd: 'shopping_cart_items',
+  shoppingCartItemsRetrieve: apiUrl + 'shopping_cart_items',
+  shoppingCartItemsAdd: apiUrl + 'shopping_cart_items',
   shoppingCartItemsRetrieveByCustomer: customerId =>
-    `shopping_cart_items/${customerId}`,
-  shoppingCartItemsUpdate: id => apiUrl + `shopping_cart_items​/${id}`,
-  shoppingCartItemsDelete: id => apiUrl + `shopping_cart_items​/${id}`,
-=======
-  productSpecificationAttributesCount: apiUrl + 'productspecificationattributes/count',
-  productSpecificationAttributesRetrieveById: id => 'productspecificationattributes/' + id,
-  productSpecificationAttributesUpdate: id => 'productspecificationattributes/' + id,
-  productSpecificationAttributesDelete: id => 'productspecificationattributes/' + id,
-
-  // ShoppingCartItems
-  shoppingCartItemsRetrieve:  apiUrl +'shopping_cart_items',
-  shoppingCartItemsAdd:  apiUrl +'shopping_cart_items',
-  shoppingCartItemsRetrieveByCustomer: customerId => 'shopping_cart_items/' + customerId,
+    'shopping_cart_items/' + customerId,
   shoppingCartItemsUpdate: id => apiUrl + 'shopping_cart_items/' + id,
   shoppingCartItemsDelete: id => apiUrl + 'shopping_cart_items/' + id,
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d
 
   // SpecificationAttributes
   specificationAttributesRetrieve: apiUrl + 'specificationattributes',
@@ -187,11 +129,5 @@ export default {
   // Store
   storeRetrieveCurrent: apiUrl + 'current_store',
   storeRetrieveAll: apiUrl + 'stores',
-<<<<<<< HEAD
   getAccessToken: apiUrl + 'getaccesstoken',
 };
-=======
-  getAccessToken: apiUrl + 'getaccesstoken'
-}
-
->>>>>>> 678e6246e1ec504f40ce546596ce8dd65bb36d8d

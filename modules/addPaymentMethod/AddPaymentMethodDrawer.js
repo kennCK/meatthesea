1483d2b8f67d1818bc189a32453811d63315a9f3
keenanmendiola <keenanpaulmendiola@gmail.com>
2020-12-04 13:28:29 +0800

    Update

--- modules/addPaymentMethod/AddPaymentMethodDrawer.js
+++ modules/addPaymentMethod/AddPaymentMethodDrawer.js
@@ -12,7 +12,7 @@ class HeaderOptions extends Component {
     super(props);
   }
   back = () => {
-    this.props.navigationProps.push('drawerStack');
+    this.props.navigationProps.pop();
   };
   render() {
     return (

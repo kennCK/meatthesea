b4a258282884f7ac65bc03f05667702ab6b4ea4f
Kennette Canales <kennettecanales@gmail.com>
2020-12-17 15:18:29 +0800

    update

--- modules/orders/PendingPickup/PendingPickupScreen.js
+++ modules/orders/PendingPickup/PendingPickupScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import styles from '../Style';
+import {connect} from 'react-redux';
 
 class PendingPickupScreen extends Component {
     redirect = (route) => {
@@ -9,31 +10,47 @@ class PendingPickupScreen extends Component {
     }
     render() {
         return (
-            <ScrollView>
-                <View style={styles.MainContainer}>
-                    <View style={styles.TopContainer}>
-                        <Text style={[styles.DescriptionContainer]}>Order number 1234</Text>
+          <ScrollView>
+            <View style={styles.MainContainer}>
+              <View style={styles.TopContainer}>
+                  <Text style={[styles.DescriptionContainer]}>Order number 1234</Text>
+              </View>
+              <View style={styles.BottomContainer}>
+                <View style={styles.SubtitleContainer}>
+                  <TouchableOpacity onPress={() => {
+                      this.redirect("returnInPersonStack")
+                  }}>
+                  
+                      <View style={[styles.LogoContainer]}>
+                          <Image resizeMode={'cover'} source={require('assets/logo1.png')} style={styles.LogoStyle} />
+                      </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => {
+                      this.redirect("requestPickupStack")
+                  }}>
+                    <View style={styles.LogoContainer}>
+                        <Image source={require('assets/logo2.png')} style={styles.LogoStyle} />
                     </View>
-                    <View style={styles.BottomContainer}>
-                        <View style={styles.SubtitleContainer}>
-                            <TouchableOpacity onPress={() => {
-                                this.redirect("returnInPersonStack")
-                            }}>
-                            
-                                <View style={[styles.LogoContainer]}>
-                                    <Image resizeMode={'cover'} source={require('assets/logo1.png')} style={styles.LogoStyle} />
-                                </View>
-                            </TouchableOpacity>
-
-                            <View style={styles.LogoContainer}>
-                                <Image source={require('assets/logo2.png')} style={styles.LogoStyle} />
-                            </View>
-                        </View>
-                    </View>
+                  </TouchableOpacity>
                 </View>
-            </ScrollView>
+              </View>
+            </View>
+          </ScrollView>
         );
     }
 }
 
-export default PendingPickupScreen;
+const mapStateToProps = state => ({state: state});
+
+const mapDispatchToProps = dispatch => {
+  const {actions} = require('@redux');
+  return {
+    setFilter: filter => dispatch(actions.setFilter(filter)),
+    setCart: cart => dispatch(actions.setCart(cart)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PendingPickupScreen);

2a460cfda99a93ad85900cdb0ecb95cc4960b9e3
BargasoRC <renanbargaso16@gmail.com>
2021-06-01 16:25:18 +0800

    success modal is changed to View

--- modules/products/components/menu.js
+++ modules/products/components/menu.js
@@ -9,7 +9,7 @@ import {
   Dimensions
 } from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faArrowLeft, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeft, faTimes, faCheck} from '@fortawesome/free-solid-svg-icons';
 import {faImage} from '@fortawesome/free-regular-svg-icons';
 import {BasicStyles, Color, Helper} from 'common';
 import Style from './style.js';
@@ -49,6 +49,21 @@ class Menu extends Component {
       addOn1: {},
       addOn2: {},
       selectedIds: [],
+      background: {
+        primary: 'rgba(0, 100, 177,.9)',
+        error: 'rgba(252, 24, 8,.9)',
+        success: 'rgba(52, 191, 69,.9)'
+      },
+      buttonColor: {
+        error: '#f5392c',
+        success: '#56d666',
+        primary: '#0785e6'
+      },
+      icon: {
+        error: faTimes,
+        success: faCheck,
+        primary: faCheck
+      }
     };
   }
 
@@ -451,6 +466,128 @@ class Menu extends Component {
     }
   }
 
+  renderAlert = (alertType, text) => {
+    return <View style={{
+      margin: 50,
+      backgroundColor: Color.white,
+      borderRadius: 20,
+      paddingBottom: 80,
+      paddingTop: 100,
+      width: 250,
+      minHeight: 200,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'absolute',
+      alignSelf: 'center',
+      zIndex: 900
+    }}>
+      <View
+        style={
+          {
+            position: 'absolute',
+            top: 0,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            height: 100,
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }
+        }
+      >
+        <View
+          style={{
+            backgroundColor: this.state.background[alertType.toLowerCase()],
+            padding: 10,
+            borderRadius: 50,
+          }}
+        >
+          <FontAwesomeIcon
+            icon={this.state.icon[alertType.toLowerCase()]}
+            size={25}
+            style={{
+              color: 'white'
+            }}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          minHeight: 80,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text style={{
+          textAlign: "center",
+          alignItems: 'center',
+          fontSize: 14,
+          color: Color.darkGray
+        }}>{text}</Text>
+      </View>
+      <View
+        style={
+          {
+            width: 100,
+            position: 'absolute',
+            bottom: 20,
+            paddingBottom: 0
+          }
+        }
+      >
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor={this.state.background[alertType.toLowerCase()]}
+          style={
+            [
+              {
+                height: 35,
+                backgroundColor: this.state.buttonColor[alertType.toLowerCase()],
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 50,
+                borderColor: 'white',
+                borderWidth: 1,
+                color: 'white',
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 2
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                elevation: 5,
+                width: 100,
+                position: 'absolute',
+                bottom: 20,
+                paddingBottom: 0
+              }
+            ]
+          }
+          onPress={()=> {
+              this.setState({ isAddingAddressName: false, visibleModal: false})
+            }
+          }
+        >
+          <Text style={{
+            color: 'white'
+          }}>Ok</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  }
+
 
   render() {
     const {restaurant, deliStore, filter, homepage, menuProducts} = this.props.state;
@@ -482,17 +619,6 @@ class Menu extends Component {
             <Text style={{fontWeight: 'bold'}}>DELI-STORE MENU</Text>
           )}
         </View>
-        <Alert
-          show={this.state.isAddingAddressName}
-          text={this.state.alertText}
-          onClick={()=> {
-            this.setState({ isAddingAddressName: false, visibleModal: false})
-            // if(!this.state.isError){
-            //   this.props.router.push('orderSummaryStack');
-            // }
-          }}
-          alertType={this.state.isError == true ? 'error' : 'primary'}
-        />
         <Modal
           isVisible={this.state.visibleModal}
           style={[
@@ -506,9 +632,15 @@ class Menu extends Component {
           }}>
           <View
             style={{
-              paddingBottom: 120
+              paddingBottom: 120,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center'
             }}
           >
+            { this.state.isAddingAddressName &&
+              this.renderAlert(this.state.isError == true ? 'error' : 'primary', this.state.alertText)
+            }
             <ScrollView 
               showsHorizontalScrollIndicator={false} 
               showsVerticalScrollIndicator={false}
